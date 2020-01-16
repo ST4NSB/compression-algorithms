720707d8012716540c4ad8b8cc86a9dd24942c05
ST4NSB <barbulescuadrian10@gmail.com>
2020-01-16 02:16:31 +0200

    added predictor'

---
+++ PredictorFormsApp/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace PredictorFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.originalPBox = new System.Windows.Forms.PictureBox();
+            this.loadOrigBttn = new System.Windows.Forms.Button();
+            this.DecodedPBox = new System.Windows.Forms.PictureBox();
+            this.encodeBttn = new System.Windows.Forms.Button();
+            this.methodComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.histogramBttn = new System.Windows.Forms.Button();
+            this.histoComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.errorPBox = new System.Windows.Forms.PictureBox();
+            this.showErrorBttn = new System.Windows.Forms.Button();
+            this.storeBttn = new System.Windows.Forms.Button();
+            this.loadEncBttn = new System.Windows.Forms.Button();
+            this.decodeBttn = new System.Windows.Forms.Button();
+            this.saveDecBttn = new System.Windows.Forms.Button();
+            this.errorTxtBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.originalPBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DecodedPBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorPBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // originalPBox
+            // 
+            this.originalPBox.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.originalPBox.Location = new System.Drawing.Point(40, 29);
+            this.originalPBox.Name = "originalPBox";
+            this.originalPBox.Size = new System.Drawing.Size(256, 256);
+            this.originalPBox.TabIndex = 0;
+            this.originalPBox.TabStop = false;
+            // 
+            // loadOrigBttn
+            // 
+            this.loadOrigBttn.Location = new System.Drawing.Point(40, 302);
+            this.loadOrigBttn.Name = "loadOrigBttn";
+            this.loadOrigBttn.Size = new System.Drawing.Size(75, 23);
+            this.loadOrigBttn.TabIndex = 1;
+            this.loadOrigBttn.Text = "Load img";
+            this.loadOrigBttn.UseVisualStyleBackColor = true;
+            this.loadOrigBttn.Click += new System.EventHandler(this.LoadOrigBttn_Click);
+            // 
+            // DecodedPBox
+            // 
+            this.DecodedPBox.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.DecodedPBox.Location = new System.Drawing.Point(757, 29);
+            this.DecodedPBox.Name = "DecodedPBox";
+            this.DecodedPBox.Size = new System.Drawing.Size(256, 256);
+            this.DecodedPBox.TabIndex = 2;
+            this.DecodedPBox.TabStop = false;
+            // 
+            // encodeBttn
+            // 
+            this.encodeBttn.Location = new System.Drawing.Point(130, 302);
+            this.encodeBttn.Name = "encodeBttn";
+            this.encodeBttn.Size = new System.Drawing.Size(75, 23);
+            this.encodeBttn.TabIndex = 3;
+            this.encodeBttn.Text = "Predict";
+            this.encodeBttn.UseVisualStyleBackColor = true;
+            this.encodeBttn.Click += new System.EventHandler(this.EncodeBttn_Click);
+            // 
+            // methodComboBox
+            // 
+            this.methodComboBox.FormattingEnabled = true;
+            this.methodComboBox.Location = new System.Drawing.Point(40, 372);
+            this.methodComboBox.Name = "methodComboBox";
+            this.methodComboBox.Size = new System.Drawing.Size(121, 21);
+            this.methodComboBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Original Img";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(754, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Decoded Img";
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(295, 351);
+            this.chart.Name = "chart";
+            this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 2;
+            series1.Name = "Freq";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(718, 296);
+            this.chart.TabIndex = 7;
+            this.chart.Text = "chart1";
+            // 
+            // histogramBttn
+            // 
+            this.histogramBttn.Location = new System.Drawing.Point(178, 566);
+            this.histogramBttn.Name = "histogramBttn";
+            this.histogramBttn.Size = new System.Drawing.Size(97, 23);
+            this.histogramBttn.TabIndex = 8;
+            this.histogramBttn.Text = "Create Histogram";
+            this.histogramBttn.UseVisualStyleBackColor = true;
+            this.histogramBttn.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // histoComboBox
+            // 
+            this.histoComboBox.FormattingEnabled = true;
+            this.histoComboBox.Location = new System.Drawing.Point(40, 568);
+            this.histoComboBox.Name = "histoComboBox";
+            this.histoComboBox.Size = new System.Drawing.Size(121, 21);
+            this.histoComboBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(386, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Error Prediction";
+            // 
+            // errorPBox
+            // 
+            this.errorPBox.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.errorPBox.Location = new System.Drawing.Point(389, 29);
+            this.errorPBox.Name = "errorPBox";
+            this.errorPBox.Size = new System.Drawing.Size(256, 256);
+            this.errorPBox.TabIndex = 10;
+            this.errorPBox.TabStop = false;
+            // 
+            // showErrorBttn
+            // 
+            this.showErrorBttn.Location = new System.Drawing.Point(390, 302);
+            this.showErrorBttn.Name = "showErrorBttn";
+            this.showErrorBttn.Size = new System.Drawing.Size(112, 23);
+            this.showErrorBttn.TabIndex = 12;
+            this.showErrorBttn.Text = "Show error matrix";
+            this.showErrorBttn.UseVisualStyleBackColor = true;
+            this.showErrorBttn.Click += new System.EventHandler(this.ShowErrorBttn_Click);
+            // 
+            // storeBttn
+            // 
+            this.storeBttn.Location = new System.Drawing.Point(221, 302);
+            this.storeBttn.Name = "storeBttn";
+            this.storeBttn.Size = new System.Drawing.Size(75, 23);
+            this.storeBttn.TabIndex = 13;
+            this.storeBttn.Text = "Store";
+            this.storeBttn.UseVisualStyleBackColor = true;
+            this.storeBttn.Click += new System.EventHandler(this.StoreBttn_Click);
+            // 
+            // loadEncBttn
+            // 
+            this.loadEncBttn.Location = new System.Drawing.Point(757, 301);
+            this.loadEncBttn.Name = "loadEncBttn";
+            this.loadEncBttn.Size = new System.Drawing.Size(85, 23);
+            this.loadEncBttn.TabIndex = 14;
+            this.loadEncBttn.Text = "Load encoded";
+            this.loadEncBttn.UseVisualStyleBackColor = true;
+            this.loadEncBttn.Click += new System.EventHandler(this.LoadEncBttn_Click);
+            // 
+            // decodeBttn
+            // 
+            this.decodeBttn.Location = new System.Drawing.Point(847, 301);
+            this.decodeBttn.Name = "decodeBttn";
+            this.decodeBttn.Size = new System.Drawing.Size(75, 23);
+            this.decodeBttn.TabIndex = 15;
+            this.decodeBttn.Text = "Decode";
+            this.decodeBttn.UseVisualStyleBackColor = true;
+            this.decodeBttn.Click += new System.EventHandler(this.DecodeBttn_Click);
+            // 
+            // saveDecBttn
+            // 
+            this.saveDecBttn.Location = new System.Drawing.Point(926, 301);
+            this.saveDecBttn.Name = "saveDecBttn";
+            this.saveDecBttn.Size = new System.Drawing.Size(87, 23);
+            this.saveDecBttn.TabIndex = 16;
+            this.saveDecBttn.Text = "Save decoded";
+            this.saveDecBttn.UseVisualStyleBackColor = true;
+            this.saveDecBttn.Click += new System.EventHandler(this.SaveDecBttn_Click);
+            // 
+            // errorTxtBox
+            // 
+            this.errorTxtBox.Location = new System.Drawing.Point(509, 303);
+            this.errorTxtBox.Name = "errorTxtBox";
+            this.errorTxtBox.Size = new System.Drawing.Size(48, 20);
+            this.errorTxtBox.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 661);
+            this.Controls.Add(this.errorTxtBox);
+            this.Controls.Add(this.saveDecBttn);
+            this.Controls.Add(this.decodeBttn);
+            this.Controls.Add(this.loadEncBttn);
+            this.Controls.Add(this.storeBttn);
+            this.Controls.Add(this.showErrorBttn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.errorPBox);
+            this.Controls.Add(this.histoComboBox);
+            this.Controls.Add(this.histogramBttn);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.methodComboBox);
+            this.Controls.Add(this.encodeBttn);
+            this.Controls.Add(this.DecodedPBox);
+            this.Controls.Add(this.loadOrigBttn);
+            this.Controls.Add(this.originalPBox);
+            this.Name = "Form1";
+            this.Text = "Predictor";
+            ((System.ComponentModel.ISupportInitialize)(this.originalPBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DecodedPBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorPBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox originalPBox;
+        private System.Windows.Forms.Button loadOrigBttn;
+        private System.Windows.Forms.PictureBox DecodedPBox;
+        private System.Windows.Forms.Button encodeBttn;
+        private System.Windows.Forms.ComboBox methodComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Button histogramBttn;
+        private System.Windows.Forms.ComboBox histoComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox errorPBox;
+        private System.Windows.Forms.Button showErrorBttn;
+        private System.Windows.Forms.Button storeBttn;
+        private System.Windows.Forms.Button loadEncBttn;
+        private System.Windows.Forms.Button decodeBttn;
+        private System.Windows.Forms.Button saveDecBttn;
+        private System.Windows.Forms.TextBox errorTxtBox;
+    }
+}
+
---
+++ PredictorFormsApp/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Predictor;
+using BitReaderWriter;
+using System.IO;
+
+namespace PredictorFormsApp
+{
+    public partial class Form1 : Form
+    {
+        const int HeaderSize = 1078;
+        const int imgLength = 256;
+       //string imagePath;
+        ImagePredictor imgPred;
+        BitReader bread;
+        BitWriter bwrite;
+        byte[,] originalImg, decodedImg;
+        FileInfo filePath;
+        List<uint> bmpHeader;
+        public Form1()
+        {
+            InitializeComponent();
+            errorTxtBox.Text = 1.ToString();
+            methodComboBox.Items.AddRange(new string[] 
+            {
+                "128",
+                "A",
+                "B",
+                "C",
+                "A+B-C",
+                "A+(B-C)/2",
+                "B+(A-C)/2",
+                "(A+B)/2",
+                "jpegLS"
+            });
+            methodComboBox.SelectedIndex = 4;
+            histoComboBox.Items.AddRange(new string[]
+            {
+                "Original",
+                "Error Prediction",
+                "Decoded"
+            });
+            histoComboBox.SelectedIndex = 0;
+        }
+
+        private void LoadOrigBttn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Images Files(*.bmp)|*.bmp";
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = open.FileName;
+                var img = Image.FromFile(fileName);
+                originalPBox.Image = new Bitmap(img);
+                img.Dispose();
+                originalImg = new byte[imgLength, imgLength];
+                for (int i = 0; i < imgLength; i++)
+                    for (int j = 0; j < imgLength; j++)
+                        originalImg[i, j] = ((Bitmap)originalPBox.Image).GetPixel(i, j).R;
+                filePath = new FileInfo(fileName);
+            }
+        }
+
+        private void EncodeBttn_Click(object sender, EventArgs e)
+        {
+            imgPred = new ImagePredictor();
+            imgPred.Encode(originalImg, methodNumber:methodComboBox.SelectedIndex);
+        }
+
+        private void LoadEncBttn_Click(object sender, EventArgs e)
+        {
+            var FD = new OpenFileDialog();
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+                filePath = new FileInfo(FD.FileName);
+            }
+        }
+
+        private Dictionary<int, int> GetFreq(byte[,] histParam)
+        {
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            for (int i = 0; i < imgLength; i++)
+                for (int j = 0; j < imgLength; j++)
+                {
+                    if (freq.ContainsKey(histParam[i, j]))
+                        freq[histParam[i, j]] += 1;
+                    else
+                        freq.Add(histParam[i, j], 1);
+                }
+            return freq;
+        }
+
+        private Dictionary<int, int> GetFreq(int[,] histParam)
+        {
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            for (int i = 0; i < imgLength; i++)
+                for (int j = 0; j < imgLength; j++)
+                {
+                    if (freq.ContainsKey(histParam[i, j]))
+                        freq[histParam[i, j]] += 1;
+                    else
+                        freq.Add(histParam[i, j], 1);
+                }
+            return freq;
+        }
+
+        private void Button1_Click(object sender, EventArgs e) // butonu de histograma
+        {
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            if (histoComboBox.SelectedIndex == 0)
+            {
+                byte[,] histParam = new byte[imgLength, imgLength];
+                histParam = originalImg;
+                freq = GetFreq(histParam);
+            }
+            else if (histoComboBox.SelectedIndex == 1)
+            {
+                int[,] histParam = new int[imgLength, imgLength];
+                histParam = imgPred.errorMatrix;
+                freq = GetFreq(histParam); ;
+            }
+            else if (histoComboBox.SelectedIndex == 2)
+            {
+                byte[,] histParam = new byte[imgLength, imgLength];
+                histParam = decodedImg;
+                freq = GetFreq(histParam);
+            }
+
+            chart.Series[0].Points.Clear();
+            foreach(var item in freq)
+                chart.Series[0].Points.AddXY(item.Key, item.Value);
+        }
+
+        private List<uint> readBMPHeader()
+        {
+            List<uint> values = new List<uint>();
+            long fileSize = 8 * new FileInfo(filePath.FullName).Length;
+            int count = 0;
+            do
+            {
+                if (count >= HeaderSize)
+                    break;
+                int readBits = 8;
+                uint value = bread.ReadNBits(readBits);
+                values.Add(value);
+                fileSize -= readBits;
+                count++;
+            } while (fileSize > 0);
+            return values;
+        }
+
+        private void StoreBttn_Click(object sender, EventArgs e)
+        {
+            bread = new BitReader(filePath.FullName);
+            bmpHeader = new List<uint>(readBMPHeader());
+            bread.Dispose();
+
+            string outputName = filePath.Name + "[" + imgPred.predictionNumber + "].pre";
+            bwrite = new BitWriter(Path.Combine(filePath.DirectoryName, outputName));
+
+            foreach (var item in bmpHeader)
+                bwrite.WriteNBits(8, item);
+                    
+            bwrite.WriteNBits(4, (uint)imgPred.predictionNumber);
+            for (int i = 0; i < imgLength; i++)
+                for (int j = 0; j < imgLength; j++)
+                {
+                    if (imgPred.errorMatrix[i, j] < 0)
+                        bwrite.WriteNBits(1, 1);
+                    else if (imgPred.errorMatrix[i, j] >= 0)
+                        bwrite.WriteNBits(1, 0);
+                    bwrite.WriteNBits(8, (uint)Math.Abs(imgPred.errorMatrix[i, j]));
+                }
+
+            bwrite.WriteNBits(7, 1);
+            bwrite.Dispose();
+        }
+
+        private List<int> readErrorMatrix()
+        {
+            List<int> values = new List<int>();
+            FileInfo fileInfo = new FileInfo(filePath.FullName);
+            long fileSize = (8 * fileInfo.Length) - (HeaderSize + 4);
+            do
+            {
+                int readBits = 1;
+                if (readBits > fileSize)
+                {
+                    readBits = (int)fileSize;
+                }
+                uint value = bread.ReadNBits(readBits);
+                int sign = (int)value;
+                fileSize -= readBits;
+
+                readBits = 8;
+                if (readBits > fileSize)
+                {
+                    readBits = (int)fileSize;
+                }
+                value = bread.ReadNBits(readBits);
+                if (sign == 0)
+                    values.Add((int)value);
+                else if (sign == 1)
+                    values.Add((-1)*(int)value);
+                fileSize -= readBits;
+            } while (fileSize > 0);
+            return values;
+        }
+
+        private void DecodeBttn_Click(object sender, EventArgs e)
+        {
+            bread = new BitReader(filePath.FullName);
+            bmpHeader = readBMPHeader();
+            int fileMethod = (int)bread.ReadNBits(4);
+            var errorList = readErrorMatrix();
+            bread.Dispose();
+
+            int[,] errorMatrix = new int[imgLength, imgLength];
+            int k = 0;
+            for (int i = 0; i < imgLength; i++)
+                for (int j = 0; j < imgLength; j++)
+                {
+                    errorMatrix[i, j] = (int)errorList[k];
+                    k++;
+                }
+
+            imgPred = new ImagePredictor();
+            imgPred.Decode(errorMatrix, methodName: fileMethod);
+            decodedImg = imgPred.origDecodedMatrix;
+
+            DecodedPBox.Image = new Bitmap(imgLength, imgLength);
+            for (int i = 0; i < imgLength; i++)
+            {
+                for (int j = 0; j < imgLength; j++)
+                {
+                    int color = decodedImg[i, j];
+                    ((Bitmap)DecodedPBox.Image).SetPixel(i, j, Color.FromArgb(color, color, color));
+                }
+            }
+            //DecodedPBox.Image.Save(filePath.DirectoryName + "\\decodedImage.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+
+        private void SaveDecBttn_Click(object sender, EventArgs e)
+        {
+            string outputName = filePath.Name + ".decoded";
+            bwrite = new BitWriter(Path.Combine(filePath.DirectoryName, outputName));
+
+            foreach (var item in bmpHeader)
+                bwrite.WriteNBits(8, item);
+
+            for (int i = imgLength - 1; i >= 0; i--)
+                for (int j = 0; j < imgLength; j++)
+                    bwrite.WriteNBits(8, decodedImg[j, i]);
+
+            //bwrite.WriteNBits(7, 1);
+            bwrite.Dispose();
+        }
+        
+        private void ShowErrorBttn_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(imgPred.errorMatrix.GetLength(0), imgPred.errorMatrix.GetLength(1));
+            for (int i = 0; i < imgLength; i++)
+                for (int j = 0; j < imgLength; j++)
+                {
+                    int value = 128 + imgPred.errorMatrix[i, j] * Convert.ToInt32(errorTxtBox.Text);
+                    if (value < 0)
+                        value = 0;
+                    else if (value > 255)
+                        value = 255;
+                    bmp.SetPixel(i, j, Color.FromArgb(value, value, value));
+                }
+            //display
+            errorPBox.Image = bmp;
+        }
+    }
+}
